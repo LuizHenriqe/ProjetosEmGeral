149bb881cbcfc44688a20481569762a3ddfaa649
Luiz Henrique <lhfl12@hotmail.com>
2022-02-14 18:00:05 -0300

    2022-02-14_18:00

--- Seção - 9 Enumerações e composição/120. Exercício resolvido 01 - PARTE 2/Course/Course/Entities/HourContract.cs
+++ Seção - 9 Enumerações e composição/120. Exercício resolvido 01 - PARTE 2/Course/Course/Entities/HourContract.cs
@@ -4,5 +4,17 @@ namespace Course.Entities {
     internal class HourContract {
         public DateTime Date { get; set; }
         public double ValuePerHour { get; set; }
+        public int Hours { get; set; }
+
+        public HourContract() { }   
+        public HourContract(DateTime date, double valuePerHour, int hours) {
+            Date = date;
+            ValuePerHour = valuePerHour;
+            Hours = hours;
+        }
+
+        public double TotalValue() {
+            return Hours * ValuePerHour;
+        }
     }
 }
---
+++ Seção - 9 Enumerações e composição/120. Exercício resolvido 01 - PARTE 2/Course/Course/Entities/Worker.cs
@@ -0,0 +1,12 @@
+﻿using Course.Entities.Enums;
+
+
+namespace Course.Entities {
+    internal class Worker {
+        public string Name { get; set; }
+
+        public WorkerLevel Level { get; set; }
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; }
+    }
+}
