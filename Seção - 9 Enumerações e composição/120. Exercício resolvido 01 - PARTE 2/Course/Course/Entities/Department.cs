742ba035b46944a940e794fd9e7cbc0ddc543b51
Luiz Henrique <lhfl12@hotmail.com>
2022-02-14 17:41:01 -0300

    20220214 - lhfl

---
+++ Seção - 9 Enumerações e composição/120. Exercício resolvido 01 - PARTE 2/Course/Course/Entities/Department.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Course.Entities {
+    internal class Department {
+        public string Name { get; set; }
+
+        public Department() { }
+
+        public Department(string name) {
+            Name = name;
+        }  
+    }
+}
---
+++ Seção - 9 Enumerações e composição/120. Exercício resolvido 01 - PARTE 2/Course/Course/Entities/HourContract.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Course.Entities {
+    internal class HourContract {
+        public DateTime Date { get; set; }
+        public double ValuePerHour { get; set; }
+    }
+}
