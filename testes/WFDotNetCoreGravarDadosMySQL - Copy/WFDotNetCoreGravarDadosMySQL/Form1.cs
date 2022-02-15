57ecb641cca76606991ca73c317299fbfd655039
Luiz Henrique <lhfl12@hotmail.com>
2022-02-15 17:17:50 -0300

    2022-02-15_17:17

---
+++ testes/WFDotNetCoreGravarDadosMySQL - Copy/WFDotNetCoreGravarDadosMySQL/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+
+namespace WFDotNetCoreGravarDadosMySQL {
+    public partial class Form1 : Form {
+
+        private MySqlConnection conexao;
+        private string data_source = "datasource=127.0.0.1;username=root;password=962392087#Luiz;database=db_agenda;";
+
+
+        public Form1() {
+            InitializeComponent();
+
+            lst_contatos.View = View.Details;
+            lst_contatos.LabelEdit = true;
+            lst_contatos.AllowColumnReorder = true;
+            lst_contatos.FullRowSelect = true;
+            lst_contatos.GridLines = true;
+
+            lst_contatos.Columns.Add("ID", 30, HorizontalAlignment.Left);
+            lst_contatos.Columns.Add("Nome", 100, HorizontalAlignment.Left);
+            lst_contatos.Columns.Add("E-mail", 100, HorizontalAlignment.Left);
+            lst_contatos.Columns.Add("Telefone", 100, HorizontalAlignment.Left);
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            try {
+
+
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
+            finally{
+                conexao.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            try {
+                
+
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
+            finally {
+                conexao.Close();
+                conexao.Dispose();
+            }
+
+        }
+    }
+}
--- testes/WFDotNetCoreGravarDadosMySQL/WFDotNetCoreGravarDadosMySQL/Form1.cs
+++ testes/WFDotNetCoreGravarDadosMySQL/WFDotNetCoreGravarDadosMySQL/Form1.cs
@@ -1,18 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using MySql.Data.MySqlClient;
+
 
 namespace WFDotNetCoreGravarDadosMySQL {
     public partial class Form1 : Form {
 
-        private MySqlConnection conexao;
+        private MySqlConnection Conexao;
         private string data_source = "datasource=127.0.0.1;username=root;password=962392087#Luiz;database=db_agenda;";
 
 
@@ -20,54 +14,65 @@ namespace WFDotNetCoreGravarDadosMySQL {
             InitializeComponent();
 
             lst_contatos.View = View.Details;
-            //lst_contatos.LabelEdit = true;
-            //lst_contatos.AllowColumnReorder = true;
-            //lst_contatos.FullRowSelect = true;
-            //lst_contatos.GridLines = true;
+            lst_contatos.LabelEdit = true;
+            lst_contatos.AllowColumnReorder = true;
+            lst_contatos.FullRowSelect = true;
+            lst_contatos.GridLines = true;
 
             lst_contatos.Columns.Add("ID", 30, HorizontalAlignment.Left);
             lst_contatos.Columns.Add("Nome", 100, HorizontalAlignment.Left);
-            lst_contatos.Columns.Add("E-mail", 100, HorizontalAlignment.Left);
             lst_contatos.Columns.Add("Telefone", 100, HorizontalAlignment.Left);
+            lst_contatos.Columns.Add("E-mail", 100, HorizontalAlignment.Left);
         }
 
         private void button1_Click(object sender, EventArgs e) {
             try {
-                //Cria conexao com MySQL  e instancia o mesmo.
-                conexao = new MySqlConnection(data_source);
+                
+                Conexao= new MySqlConnection(data_source);
 
-                string sql = "INSERT INTO contato(nome, telefone, email) " +
-                "VALUES('" + txt_nome.Text + "','" + txt_telefone.Text + "','" + txt_email.Text + "'); ";
+                Conexao.Open();
+                
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.Connection = Conexao;
 
-                conexao.Open();
+                cmd.Parameters.AddWithValue("@nome",txt_nome.Text);
+                cmd.Parameters.AddWithValue("@telefone", txt_telefone.Text);
+                cmd.Parameters.AddWithValue("@email", txt_email.Text);
 
-                // Executa comando INSERT
-                MySqlCommand comando = new MySqlCommand(sql,conexao);
+                cmd.CommandText = "INSERT INTO contato (nome, telefone, email) " +
+                                  "VALUES (@nome,@telefone,@email)";
+                cmd.Prepare();
 
-                comando.ExecuteReader();
 
-                MessageBox.Show("Comando inserido com sucesso " + sql);
+                cmd.ExecuteNonQuery();
 
+                MessageBox.Show("Contato inserido com sucesso!","Sucesso!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+            catch (MySqlException ex) {
+                MessageBox.Show("Erros "+ ex.Number +" socorreu: "+ ex.Message,
+                                "Erro ", MessageBoxButtons.OK,MessageBoxIcon.Error);
             }
             catch (Exception ex) {
-                MessageBox.Show(ex.Message);
+                MessageBox.Show("ocorreu: " + ex.Message,
+                                "Error ", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
-            finally{
-                conexao.Close();
+            finally {
+                Conexao.Close();
             }
         }
 
         private void button2_Click(object sender, EventArgs e) {
             try {
-                conexao = new MySqlConnection(data_source);
+                Conexao = new MySqlConnection(data_source);
 
                 string q = " '%"+txt_buscar.Text+"%' ";
                 string sql = "SELECT * FROM contato WHERE nome LIKE"+q+"OR email LIKE"+q;
 
-                conexao.Open();
+                Conexao.Open();
 
                 //vincular comando.
-                MySqlCommand comando = new MySqlCommand(sql, conexao);
+                MySqlCommand comando = new MySqlCommand(sql, Conexao);
 
                 MySqlDataReader reader = comando.ExecuteReader();
 
@@ -92,8 +97,8 @@ namespace WFDotNetCoreGravarDadosMySQL {
                 MessageBox.Show(ex.Message);
             }
             finally {
-                conexao.Close();
-                conexao.Dispose();
+                Conexao.Close();
+                Conexao.Dispose();
             }
 
         }
