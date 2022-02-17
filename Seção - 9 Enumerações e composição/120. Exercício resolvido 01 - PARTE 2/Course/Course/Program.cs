32f59600ad52b493c35efa1b67d26351b1446478
Luiz Henrique <77198741+LuizHenriqe@users.noreply.github.com>
2022-02-16 21:55:13 -0300

    2022-02-16_21:55

--- Seção - 9 Enumerações e composição/120. Exercício resolvido 01 - PARTE 2/Course/Course/Program.cs
+++ Seção - 9 Enumerações e composição/120. Exercício resolvido 01 - PARTE 2/Course/Course/Program.cs
@@ -1,9 +1,47 @@
 ﻿using System;
+using Course.Entities.Enums;
+using Course.Entities;
+using System.Globalization;
 
 namespace Course {
     internal class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter Department's name: ");
+            string deptName = Console.ReadLine();
+            Console.WriteLine("Enter worker data:");
+            Console.Write("Name: ");
+            string name =Console.ReadLine();
+
+            Console.Write("Level (Junior/MidLevel/Senior): ");
+            WorkerLevel level = Enum.Parse<WorkerLevel>(Console.ReadLine());
+            
+            Console.Write("BAse salary: ");
+            double basesalary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture   );
+
+            Department dept = new Department(deptName);
+
+            Worker worker = new Worker(name,level,basesalary,dept);
+
+            Console.Write("how many contracts to  this worker?");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i =1; i < n; i++)
+            {
+                Console.WriteLine($"Enter  #{i} contract data: ");
+                Console.Write("Date (DD/MM/YYYY): ");
+                DateTime date = DateTime.Parse(Console.ReadLine());
+
+                Console.Write("Value per hour:");
+                double valuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Duration (HOurs): ");
+                int hours = int.Parse(Console.ReadLine());
+
+                HourContract contract = new HourContract(date, valuePerHour, hours);
+
+                worker.AddContract(contract);
+
+            }
+
         }
     }
 }
