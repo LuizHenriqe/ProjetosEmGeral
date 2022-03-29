b5518557722f96a4b2d8887d6c26d756db7c2873
Luiz Henrique <77198741+LuizHenriqe@users.noreply.github.com>
2022-03-28 21:33:55 -0300

    Aula 135 - heranca
    Aula sobre herança

---
+++ Seção - 10 Heranca e polimorfismo/135. heranca/Course/Course/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Course.Entites;
+
+namespace Course {
+    internal class Program {
+        static void Main(string[] args) {
+
+            BusinessAccount account = new BusinessAccount(100,"Henrique", 50000,500);
+
+            Console.WriteLine(account.Balance);
+
+        }
+    }
+}
