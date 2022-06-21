8b498fcdc83ae521722ce4200ba17b217bd5dfa7
Luiz Henrique <77198741+LuizHenriqe@users.noreply.github.com>
2022-06-21 15:25:07 -0300

    update
    pastas atualizadas

---
+++ Dev/CadUsuarios_SqliteModel/Programa/Programa/Migrations/20210713005841_migration_data.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Programa.Migrations
+{
+    public partial class migration_data : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SetUsuarios",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    NAME = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SetUsuarios", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SetUsuarios");
+        }
+    }
+}
---
+++ Dev/CadUsuarios_SqliteModel/Programa/Programa/Models/Usuario.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programa.Models {
+    class Usuario {
+        public int ID { get; set; }
+        public string NAME { get; set; }
+    }
+}
