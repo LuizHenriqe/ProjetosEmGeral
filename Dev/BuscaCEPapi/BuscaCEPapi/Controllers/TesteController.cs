e1f9a9500650d754969a75d206f9af7aa24cfd3c
Luiz Henrique <lhfl12@hotmail.com>
2022-10-31 15:59:06 -0300

    atua
    atua

---
+++ Dev/BuscaCEPapi/BuscaCEPapi/Controllers/TesteController.cs
@@ -0,0 +1,65 @@
+﻿using BuscaCEPapi.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Refit;
+using System;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace BuscaCEPapi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TesteController : Controller
+    {
+        [HttpGet]
+        [Route("{cep}")]
+        public async Task<IActionResult> testaCEP([FromRoute]string cep)
+        {
+            try
+            {
+                var CepClient = RestService.For<ICepApiService>("http://viacep.com.br");
+
+                var Address = await CepClient.GetAddressAsync(cep);
+                
+                CepResponse re = Address;
+
+                SmtpClient client = new SmtpClient("smtp-mail.outlook.com",587);
+
+
+                client.Host = "smtp-mail.outlook.com";
+                client.EnableSsl = true;
+                client.Credentials = new NetworkCredential("lhfl12@hotmail.com", "Nissan+-.123");
+                client.DeliveryMethod = SmtpDeliveryMethod.Network;
+
+                MailMessage mail = new MailMessage();
+                mail.Sender = new MailAddress("lhfl12@hotmail.com");
+                mail.From = new MailAddress("lhfl12@hotmail.com");
+                mail.To.Add(new MailAddress("lhfl@updi.net","Luiz Henrique"));
+                mail.Subject = "Luiz";
+                mail.Body = " endereço: " + re;
+                mail.IsBodyHtml = true;
+                
+                try
+                {
+                    client.Send(mail);
+                }catch (Exception ex)
+                {
+                    return BadRequest(ex.Message);
+                }
+                finally
+                {
+                    client.Dispose();
+                }
+
+                return Ok(Address);
+
+
+            }
+            catch(Exception ex)
+            {
+                return BadRequest("Erro " + Convert.ToString(ex.Message));
+            }
+        }
+    }
+}
---
+++ Dev/BuscaCEPapi/BuscaCEPapi/Entities/CepResponse.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System.Runtime.Serialization;
+
+namespace BuscaCEPapi.Entities
+{
+    public class CepResponse
+    {
+        [JsonProperty(propertyName: "cep")]
+        public string Cep { set; get; }
+        
+        [JsonProperty("logradouro")]
+        public string Logradouro { set; get; }
+        
+        [JsonProperty("complemento")]
+        public string Complemento { set; get; }
+        
+        [JsonProperty("bairro")]
+        public string Bairro{ set; get; }
+        
+        [JsonProperty("localidade")]
+        public string Localidade { set; get; }
+        
+        [JsonProperty("uf")]
+        public string Uf { set; get; }
+        
+        [JsonProperty("unidade")]
+        public string Unidade { set; get; }
+        
+        [JsonProperty("ibge")]
+        public string Ibge { set; get; }
+        
+        [JsonProperty("gia")]
+        public string Gia { set; get; }
+
+
+        public override string ToString()
+        {
+            return $"Cep: {Cep}, Logradouro:{Logradouro}, Bairro:{Bairro}";
+        }
+
+    }
+
+
+}
